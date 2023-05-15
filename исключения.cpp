2b8c71d9cc673bc11b909623b24d62564c8205ec
Shmelsergey <131392590+Shmelsergey@users.noreply.github.com>
2023-05-15 22:07:40 +0400

    Add files via upload

---
+++ исключения.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include "IntArray.h"
+
+int main()
+{
+
+    // Declare an array with 10 elements
+    IntArray array(10);
+    
+
+    // Fill the array with numbers 1 through 10
+    for (int i{ 0 }; i < 10; ++i)
+        array[i] = i + 1;
+    try {
+    // Resize the array to 8 elements
+    array.resize(8);
+
+    // Insert the number 20 before element with index 5
+    
+        array.insertBefore(20, 5);
+
+        // Remove the element with index 3
+        array.remove(3);
+    }
+    catch (...)
+    {
+        //std::cerr << ex.what() << std::endl;
+    }
+    // Add 30 and 40 to the end and beginning
+    array.insertAtEnd(30);
+    array.insertAtBeginning(40);
+
+    // A few more tests to ensure copy constructing / assigning arrays
+    // doesn't break things
+    {
+        IntArray b{ array };
+        b = array;
+        b = b;
+        array = array;
+    }
+
+    // Print out all the numbers
+    try {
+    for (int i{ 0 }; i < array.getLength(); ++i)
+        
+        std::cout << array[i] << ' ';
+    }
+
+    catch (...)
+    {
+        //std::cerr << ex.what() << std::endl;
+    }
+    std::cout << '\n';
+
+    return 0;
+}
